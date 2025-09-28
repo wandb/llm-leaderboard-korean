bbf24bb74bab13085d58a104575b0055c884429a
hw-oh <hyunwoo.oh@wandb.com>
2025-09-28 10:04:35 -0400

    WIP

--- llm_eval/evaluation/__init__.py
+++ llm_eval/evaluation/__init__.py
@@ -39,3 +39,4 @@ from .llm_judge import LLMJudgeEvaluator
 from .math_eval import MathMatchEvaluator
 from .partial_match import PartialMatchEvaluator
 from .log_prob import LogProbEvaluator
+from .ifeval import IFEvalStrictEvaluator, IFEvalLooseEvaluator
