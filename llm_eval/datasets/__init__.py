ae3d43defdcac795284a8b52e71424b86bf28b7d
hw-oh <hyunwoo.oh@wandb.com>
2025-10-14 14:35:44 +0900

    Add haerae v1, squad_kor_v1 dataset

--- llm_eval/datasets/__init__.py
+++ llm_eval/datasets/__init__.py
@@ -54,3 +54,5 @@ from .korean_sat import KoreanSATDataset
 from .ifeval_ko import IFEvalKoDataset
 from .mrcr import MRCRDataset
 from .halluLens import HalluLensDataset
+from .squad_kor_v1 import SQuADKorV1
+from .haerae_v1 import HaeraeDatasetV1
