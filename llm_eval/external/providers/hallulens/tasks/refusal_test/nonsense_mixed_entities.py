f9faba19866fb465d125bb1be0b1f45061280a9b
hw-oh <hyunwoo.oh@wandb.com>
2025-10-23 19:19:45 +0900

    Enhance BFCL & HalluLens evaluation with LLMEval adapter

--- llm_eval/external/providers/hallulens/tasks/refusal_test/nonsense_mixed_entities.py
+++ llm_eval/external/providers/hallulens/tasks/refusal_test/nonsense_mixed_entities.py
@@ -16,8 +16,8 @@ from llm_eval.external.providers.hallulens.tasks.refusal_test.entities_generatio
 
 
 class NonsenseMixedInference(NonsenseNameInference):
-    def __init__(self, taskname, output_base_dir, generate_model, prompt_path, seed, method='vllm', limit=None):
-        super().__init__(output_base_dir, generate_model, prompt_path, seed, method, limit=limit)
+    def __init__(self, taskname, output_base_dir, generate_model, prompt_path, seed, method='vllm', limit=None, backend_kwargs=None):
+        super().__init__(output_base_dir, generate_model, prompt_path, seed, method, limit=limit, backend_kwargs=backend_kwargs)
 
         self.output_base_dir = output_base_dir
         self.generate_model = generate_model
