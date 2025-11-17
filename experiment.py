9021fb5071649724845828348d4f99d5770a9ce4
hw-oh <hyunwoo.oh@wandb.com>
2025-11-17 11:07:17 +0000

    Update model configurations and add new models for evaluation

--- experiment.py
+++ experiment.py
@@ -1,32 +1,37 @@
 import os
 
-korean_configs = [
-    "EXAONE-4.0.1-32B",
-    "EXAONE-4.0-32B"
+lgai_configs = [
+    # "EXAONE-4.0-1.2B",
+    # "EXAONE-4.0.1-32B",
+    # "EXAONE-4.0-32B"
 ]
 
 openai_configs = [
     # "gpt-4o-2024-11-20",
     # "gpt-4.1-2025-04-14",
-    "gpt-5-mini-2025-08-07",
-    "gpt-5-2025-08-07",
-    "o4-mini-2025-04-16",
+    "gpt-5-nano-2025-08-07",
+    # "gpt-5-mini-2025-08-07",
+    # "gpt-5-2025-08-07",
+    # "o4-mini-2025-04-16",
 ]
 
 anthropic_configs = [
     "claude-sonnet-4-5-20250929",
     "claude-haiku-4-5-20251001",
+    "claude-opus-4-1-20250805",
+    "claude-opus-4-20250514"
 ]
 
 xai_configs = [
-    "grok-4-fast-non-reasoning",
-    "grok-4-fast-reasoning",
-    "grok-4-0709",
+    # "grok-4-fast-non-reasoning",
+    # "grok-4-fast-reasoning",
+    # "grok-4-0709",
 ]
 
 google_configs = [
-    "gemini-2.5-flash",
-    "gemini-2.5-pro",
+    "gemini-2.5-flash-lite",
+    # "gemini-2.5-flash",
+    # "gemini-2.5-pro",
 ]
 
 print("\n\n-----------------------------------------------------")
@@ -34,8 +39,6 @@ print("-----------------------------------------------------")
 print("Running evaluations for all specified model configurations...")
 print("-----------------------------------------------------")
 print("-----------------------------------------------------\n\n")
-for config in google_configs:#openai_configs + xai_configs:# anthropic_configs + openai_configs + xai_configs:
-    print("\n\n-----------------------------------------------------")
-    print(f"||||   python run_eval.py --config {config}   ||||")
-    print("-----------------------------------------------------\n\n")
+for config in lgai_configs+openai_configs+anthropic_configs+xai_configs+google_configs:#openai_configs + xai_configs:# anthropic_configs + openai_configs + xai_configs:
+    print(f"||||   python run_eval.py --config {config}   ||||\n\n")
     os.system(f"python run_eval.py --config {config}")
